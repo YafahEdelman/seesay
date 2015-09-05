46c7b9919246d0c99c7696357387c631253f4e72
Alok Tripathy <kullalok@gmail.com>
2015-09-05 13:11:52 -0400

    Updated serving static files.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
