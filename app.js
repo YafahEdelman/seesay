29673f749070d31e05b5b9978fc222688cf097f3
JacobEdelman <edelman.jd@gmail.com>
2015-09-05 18:39:47 -0400

    Debugging.

--- app.js
+++ app.js
@@ -11,22 +11,12 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 var testing = false;
-
-function get_sentence(file_location, callback){
-
-    python("sentence_maker.get_sentence('"+file_location+"')", function(err, data) {
-        if (err) throw err;
-        callback(data)
-
-    });
-}
-
-
 if (testing) {
     http.listen(80, function() {
         console.log('Listening on *:80.');
     });
 } else {
+
     python("import sys;sys.path.append('jacob-neuraltalk');import sentence_maker", function(err, data) {
         if (err) throw err;
         // get_data("../caffe/examples/images", "cat cycle", console.log);
@@ -36,25 +26,37 @@ if (testing) {
         http.listen(80, function() {
             console.log('Listening on *:80.');
         });
-        io.on('connection', function(socket) {
-            socket.on('picture', function(data) {
-                console.log('received image');
-                var data = data.replace(/^data:image\/\w+;base64,/, "");
-                var buff = new Buffer(data, 'base64');
-                var fname = __dirname + '/jacob-neuraltalk/images/snapshot' + socket.id + '.png';
-                console.log(fname);
-                var fd = fs.openSync(fname, 'w');
-                fs.write(fd, buff, 0, buff.length, 0, function(err, written) {console.log(err)});
-                // CLOSE SOMETHING HERE?
-                if (!testing) {
-                    console.log("sentence_maker.get_sentence('images/snapshot" + socket.id + ".png')");
-                    console.log("over here")
-                    get_sentence("sentence_maker.get_sentence('images/snapshot" + socket.id + ".png')",
-                                function(x){console.log(x); fs.unlink(fname, function(err) {});})
-
-                }
-            });
-
-        });
     });
 }
+
+// python("sentence_maker.here()",console.log)
+//
+//
+// function get_sentence(file_location, callback){
+//
+//     python("sentence_maker.get_sentence('"+file_location+"')", function(err, data) {
+//         if (err) throw err;
+//         callback(data)
+//
+//     });
+// }
+// io.on('connection', function(socket) {
+//     socket.on('picture', function(data) {
+//         console.log('received image');
+//         var data = data.replace(/^data:image\/\w+;base64,/, "");
+//         var buff = new Buffer(data, 'base64');
+//         var fname = __dirname + '/jacob-neuraltalk/images/snapshot' + socket.id + '.png';
+//         console.log(fname);
+//         var fd = fs.openSync(fname, 'w');
+//         fs.write(fd, buff, 0, buff.length, 0, function(err, written) {console.log(err)});
+//         // CLOSE SOMETHING HERE?
+//         if (!testing) {
+//             console.log("sentence_maker.get_sentence('images/snapshot" + socket.id + ".png')");
+//             console.log("over here")
+//             get_sentence("sentence_maker.get_sentence('images/snapshot" + socket.id + ".png')",
+//                         function(x){console.log(x); fs.unlink(fname, function(err) {});})
+//
+//         }
+//     });
+//
+// });
