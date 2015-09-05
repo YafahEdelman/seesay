466c970a7d87ab2dc3e913c83e31cd1da0ef6b82
JacobEdelman <edelman.jd@gmail.com>
2015-09-05 18:42:08 -0400

    Debugging.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ if (testing) {
     });
 } else {
 
-    python("1+1", function(err, data) {
+    python("import sys;sys.path.append('jacob-neuraltalk');import sentence_maker;1+1", function(err, data) {
         if (err) throw err;
         // get_data("../caffe/examples/images", "cat cycle", console.log);
         console.log(data);
