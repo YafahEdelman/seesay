70259895ffbeaffb15389aa273f4bca4568f606f
JacobEdelman <edelman.jd@gmail.com>
2015-09-05 13:39:32 -0400

    Added jacob-neuraltalk properly.

---
+++ jacob-neuraltalk/run.sh
@@ -0,0 +1,5 @@
+CUDA_HOME=/usr/local/cuda-7.0
+LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64
+ipython notebook
+#python py_caffe_feat_extract.py -o out --model_path ~/caffe/models/VGG_ILSVRC_16_layers/VGG_ILSVRC_16_layers.caffemodel --model_def_path matlab_features_reference/deploy_features.prototxt -i example_images/ -i test
+#python py_caffe_feat_extract.py -o out --model_path ~/caffe/models/VGG_ILSVRC_16_layers/VGG_ILSVRC_16_layers.caffemodel --model_def_path ~/caffe/models/VGG_ILSVRC_16_layers/VGG_ILSVRC_16_layers_deploy.prototxt -i example_images/ -i test
---
+++ jacob-neuraltalk/sentence_maker.py
@@ -0,0 +1,32 @@
+import predict_on_images
+import py_caffe_feat_extract
+import os
+word_data_file_name =  "synset_words.txt"
+word_data_file = open(word_data_file_name)
+word_data = []
+for i in word_data_file.read().split("\n"):
+    if " " in i:
+        word_data.append(i[i.index(" ") + 1:])
+word_data_file.close()
+
+def get_objects(predictions, threshold = 0.01):
+    rets = []
+    for chance, word in zip(predictions, word_data):
+        if chance >= threshold:
+            rets.append(word)
+    return rets
+
+def get_sentence(file_path):
+    word_predictions = py_caffe_feat_extract.gen_feats(file_path, "temp_feats")
+    ret = predict_on_images.get_sentences(file_path, "temp_feats")
+    os.remove("temp_feats")
+    objects = get_objects(word_predictions)
+
+    if len(objects) == 0:
+        objects_sentence = "There are no visible objects."
+    elif len(objects) == 1:
+        objects_sentence =  "There is a %s."%objects[0]
+    else:
+        objects_sentence =  "There is a " + ", ".join(objects[:-1]) + ", and %s."%objects[-1]
+
+    return ret + ". " + objects_sentence
