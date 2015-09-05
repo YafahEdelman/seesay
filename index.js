95d94872fe4ad3188e3157904174b801babd5663
Ben Edelman <benedelman100@gmail.com>
2015-09-05 19:12:19 -0400

    browser compatibility (put everything in onload function)

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-console.log(window.location.host)
-var socket = io.connect(window.location.host);
+window.onload = ready;
 
-var streaming = false,
-    video = document.querySelector('#video'),
-    canvas = document.querySelector('#canvas'),
-    startbutton = document.querySelector('#startbutton'),
-    width = 250,
-    height = 0;
+function ready() {
+    console.log(window.location.host)
+    var socket = io.connect(window.location.host);
 
-navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    var streaming = false,
+        video = document.querySelector('#video'),
+        canvas = document.querySelector('#canvas'),
+        startbutton = document.querySelector('#startbutton'),
+        width = 250,
+        height = 0;
 
-navigator.getMedia({
-    video: true,
-    audio: false
-},
+    navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 
-function (stream) {
-    if (navigator.mozGetUserMedia) {
-        video.mozSrcObject = stream;
-    } else {
-        var vendorURL = window.URL || window.webkitURL;
-        video.src = vendorURL.createObjectURL(stream);
+    navigator.getMedia({
+        video: true,
+        audio: false
+    },
+
+    function (stream) {
+        if (navigator.mozGetUserMedia) {
+            video.mozSrcObject = stream;
+        } else {
+            var vendorURL = window.URL || window.webkitURL;
+            video.src = vendorURL.createObjectURL(stream);
+        }
+        video.play();
+    },
+
+    function (err) {
+        console.log("An error occured! " + err);
+    });
+
+    video.addEventListener('canplay', function (ev) {
+        if (!streaming) {
+            height = video.videoHeight / (video.videoWidth / width);
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+            streaming = true;
+        }
+    }, false);
+
+    function takepicture() {
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+        var data = canvas.toDataURL('image/png');
+        console.log(data);
+        socket.emit('picture', data);
+        responsiveVoice.speak("I wonder if Alok can hear this.", "US English Female");
     }
-    video.play();
-},
 
-function (err) {
-    console.log("An error occured! " + err);
-});
-
-video.addEventListener('canplay', function (ev) {
-    if (!streaming) {
-        height = video.videoHeight / (video.videoWidth / width);
-        video.setAttribute('width', width);
-        video.setAttribute('height', height);
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        streaming = true;
-    }
-}, false);
-
-function takepicture() {
-    canvas.width = width;
-    canvas.height = height;
-    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
-    var data = canvas.toDataURL('image/png');
-    console.log(data);
-    socket.emit('picture', data);
-    responsiveVoice.speak("I wonder if Alok can hear this.", "US English Female");
+    startbutton.addEventListener('click', function (ev) {
+        takepicture();
+        ev.preventDefault();
+    }, false);
 }
-
-startbutton.addEventListener('click', function (ev) {
-    takepicture();
-    ev.preventDefault();
-}, false);
